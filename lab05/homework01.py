21f5fe10a1931a1683b1c399ad9a5950e83cdb85
Zarmin <raimovazarminakhon@gmail.com>
2025-03-14 22:02:37 +0600

    Completed lab05

---
+++ lab05/homework01.py
@@ -0,0 +1,25 @@
+#1. Name variable
+name = "Zarmina"
+print("Name:", name)
+
+#2. Arithmetic operations
+a = 15
+b = 5
+print("Addition:", a + b)
+print("Subtraction:", a - b)
+print("Multiplication:", a * b)
+print("Division:", a / b)
+
+#3.Swap variables
+x, y = 3, 7
+x, y = y, x
+print("After swapping: x=", x, ", y=", y)
+
+#4. Circle area calculation
+PI = 3.14159
+radius = 9
+area = PI * (radius ** 2)
+print("Area of circle: ", area)
+
+
+
