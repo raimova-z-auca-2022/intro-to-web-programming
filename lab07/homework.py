97e905df6bd33d8ec88d38f68b5a75af22bc5623
Zarmin <raimovazarminakhon@gmail.com>
2025-03-14 22:11:51 +0600

    Completed lab07

---
+++ lab07/homework.py
@@ -0,0 +1,19 @@
+# Print numbers from 1 to 10 using for loop
+for i in range(1, 11):
+    print(i)
+
+# Print numbers from 1 to 10 using while loop
+num = 1
+while num <= 10:
+    print(num)
+    num += 1
+
+# Iterate over the string "Python" and print each character
+for char in "Python":
+    print(char)
+
+# Print even numbers from 2 to 10 using while loop
+num = 2
+while num <= 10:
+    print(num)
+    num += 2
